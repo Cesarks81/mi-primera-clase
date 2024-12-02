eddc128661233c929fb2198ea949a73a150e70e7
cesar <cesarks81@gmail.com>
2024-12-02 12:57:37 +0100

    Comit inicial

---
+++ AperturaAjedred.java
@@ -0,0 +1,27 @@
+public class AperturaAjedred{
+    
+    public String nombreApertura;
+    public int movimientosDeLibro;
+    public boolean esVigente;
+    
+    public AperturaAjedred(String nombre, int movimiento, boolean vigencia){
+    nombreApertura = nombre;
+    movimientosDeLibro = movimiento;
+    esVigente = true;
+    }
+
+    public String getNombre (){ return nombreApertura;}
+    public int getMovimiento (){ return movimientosDeLibro;}
+    public boolean getVigencia (){ return esVigente;}
+    
+    public void setNombre(String nombre){
+        nombreApertura = nombre;
+    }
+    public void setMovimientos(int movimiento){
+        movimientosDeLibro = movimiento/2+movimientosDeLibro;
+    }
+    public void setVigencia(){
+        if(esVigente == true){esVigente = false;}
+        else {esVigente = true;}
+    }
+}
